6a3791e1fa820452012997937c0b9ec389aa56cc
ferdoms <marinhosilva.fernando@gmail.com>
2019-08-07 22:15:33 +0100

    folder structure re-arranged

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,40 +1,36 @@
 import express from "express";
 import { createDbConnection } from "./db";
 import bodyParser = require("body-parser");
-import {getAuthController} from './backend/controller/auth_controller';
-import {getUserController} from './backend/controller/user_controller';
-import { getLinkController } from "./backend/controller/link_controller";
-import { getCommentController } from "./backend/controller/comment_controller";
+import { getAuthController } from "./controller/auth_controller";
+import { getUserController } from "./controller/user_controller";
+import { getLinkController } from "./controller/link_controller";
+import { getCommentController } from "./controller/comment_controller";
 
-export async function application(){
+export async function application() {
+  // Create db connection
+  await createDbConnection();
 
-    // Create db connection
-    await createDbConnection();
+  // Creates app
+  const app = await express();
 
-    // Creates app
-    const app = await express();
+  // Server config to be able to send JSON
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-    // Server config to be able to send JSON
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+  // Declare main path
+  app.get("/api/v1", (req, res) => {
+    res.send("This is the home page!");
+  });
 
-    // Declare main path
-    app.get("/api/v1", (req, res) => {
-        res.send("This is the home page!");
-    });
+  // Declare controllers
+  const usersController = getUserController();
+  const authController = getAuthController();
+  const linksController = getLinkController();
+  const commentsController = getCommentController();
+  app.use("/api/v1/auth", authController);
+  app.use("/api/v1/users", usersController);
+  app.use("/api/v1/links", linksController);
+  app.use("/api/v1/comments", commentsController);
 
-
-    // Declare controllers
-    const usersController = getUserController();
-    const authController = getAuthController();
-    const linksController = getLinkController();
-    const commentsController = getCommentController();
-    app.use("/api/v1/auth", authController);
-    app.use("/api/v1/users", usersController);
-    app.use("/api/v1/links", linksController);
-    app.use("/api/v1/comments", commentsController);
-
-
-    return app;
-    
-};
+  return app;
+}
